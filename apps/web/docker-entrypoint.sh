af1f084df7b98b8790c4a0c78c772b1b3ff1303b
Sagarworkhub <sagarbarpete3@gmail.com>
2025-07-05 02:39:17 +0530

    chore: update Caddyfile and entrypoint script to use VITE_SERVER_URL for API and TRPC requests

--- apps/web/docker-entrypoint.sh
+++ apps/web/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Replace environment variables in Caddyfile
-sed -i "s|\{\\$BACKEND_URL\}|${BACKEND_URL}|g" /etc/caddy/Caddyfile
+sed -i "s|\{\\$VITE_SERVER_URL\}|${VITE_SERVER_URL}|g" /etc/caddy/Caddyfile
 sed -i "s|\{\\$PORT\}|${PORT}|g" /etc/caddy/Caddyfile
 
 echo "Starting Caddy on port ${PORT}..."
